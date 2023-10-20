7b4049f7d58437a158ebeac3ac735c5f691c931b
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2023-10-21 04:30:08 +0700

    hw-week10

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -5,12 +5,16 @@ const router = express.Router();
 
 // router.get("/", MovieController.AllMovies);
 router.get("/all", MovieController.getAllMovies);
+
 router.get("/add", MovieController.createForm);
 router.post("/add", upload, MovieController.createMovie);
+
 router.get("/edit/:id", MovieController.updateForm);
 router.post("/edit/:id", upload, MovieController.updateMovie);
+
 router.get("/delete/:id", MovieController.deleteForm);
 router.post("/delete/:id", MovieController.deleteMovie);
+
 router.get("/uploads/:filename", MovieController.getUploadedFile);
 
 module.exports = router;
