478c9d78af4d19a30b9eddd43c8357ddc43e0baf
Rizki Rifani <rizkirifandi7@gmail.com>
2023-10-22 08:39:48 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,80 +1,65 @@
-setTimeout(function () {
-	var successMessage = document.getElementById("successMessage");
-	if (successMessage) {
-		successMessage.style.display = "none";
-	}
-}, 2000);
+function hideMessage(messageId, closeButtonId) {
+    setTimeout(function () {
+        var message = document.getElementById(messageId);
+        if (message) {
+            message.style.display = "none";
+        }
+    }, 2000);
 
-var closeSuccessMessage = document.getElementById("closeSuccessMessage");
-if (closeSuccessMessage) {
-	closeSuccessMessage.addEventListener("click", function () {
-		var successMessage = document.getElementById("successMessage");
-		if (successMessage) {
-			successMessage.style.display = "none";
-		}
-	});
+    var closeButton = document.getElementById(closeButtonId);
+    if (closeButton) {
+        closeButton.addEventListener("click", function () {
+            var message = document.getElementById(messageId);
+            if (message) {
+                message.style.display = "none";
+            }
+        });
+    }
 }
 
-setTimeout(function () {
-	var errorMessages = document.getElementById("errorMessages");
-	if (errorMessages) {
-		errorMessages.style.display = "none";
-	}
-}, 2000);
+hideMessage("successMessage", "closeSuccessMessage");
+hideMessage("errorMessages", "closeErrorMessages");
 
-var closeErrorMessages = document.getElementById("closeErrorMessages");
-if (closeErrorMessages) {
-	closeErrorMessages.addEventListener("click", function () {
-		var errorMessages = document.getElementById("errorMessages");
-		if (errorMessages) {
-			errorMessages.style.display = "none";
-		}
-	});
-}
+const tablesToInitialize = ["usersTable", "moviesTable"];
 
 $(document).ready(function () {
-	$("#usersTable").DataTable();
+    tablesToInitialize.forEach(function (tableId) {
+        $("#" + tableId).DataTable();
+    });
 });
 
-$(document).ready(function () {
-	$("#moviesTable").DataTable();
-});
+const navItems = {
+    "/users/all": "users-nav",
+    "/movies/all": "movies-nav",
+};
 
-// navbar nav menu otomatis
-// Mengambil pathname dari URL
 const pathName = window.location.pathname;
+const currentNavItem = navItems[pathName];
 
-// Menyimpan elemen navbar
-const usersNav = document.getElementById("users-nav");
-const moviesNav = document.getElementById("movies-nav");
-
-// Mengatur kelas bold pada elemen yang sesuai dengan halaman saat ini
-if (pathName === "/users/all") {
-	usersNav.classList.add("bold");
-} else if (pathName === "/movies/all") {
-	moviesNav.classList.add("bold");
+if (currentNavItem) {
+    const navElement = document.getElementById(currentNavItem);
+    if (navElement) {
+        navElement.classList.add("bold");
+    }
 }
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-	"use strict";
+    "use strict";
 
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	const forms = document.querySelectorAll(".needs-validation");
+    const forms = document.querySelectorAll(".needs-validation");
 
-	// Loop over them and prevent submission
-	Array.from(forms).forEach((form) => {
-		form.addEventListener(
-			"submit",
-			(event) => {
-				if (!form.checkValidity()) {
-					event.preventDefault();
-					event.stopPropagation();
-				}
+    Array.from(forms).forEach((form) => {
+        form.addEventListener(
+            "submit",
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
 
-				form.classList.add("was-validated");
-			},
-			false
-		);
-	});
+                form.classList.add("was-validated");
+            },
+            false
+        );
+    });
 })();
